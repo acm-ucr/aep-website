877a55276b8fc5e8f097fb2b2edd091f1f7751c4
Ronald Fan <rfan018@ucr.edu>
2026-07-04 15:19:54 -0700

    Complete Issue #4

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,23 @@
+import Image from "next/image";
+import AEPLogo from "@/public/Logo.webp";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="fixed top-0 left-0 z-50 flex h-25 w-full items-center">
+      <div className="flex flex-1 items-center p-4">
+        <Image src={AEPLogo} alt="AEP Logo" className="size-15 ml-6" />
+        <p className="ml-6 text-4xl font-extrabold whitespace-nowrap text-black">
+          AEP
+        </p>
+      </div>
+      <nav className="flex items-center gap-8 pr-8 text-2xl font-bold text-black">
+        <a href="#">About</a>
+        <a href="#">Board</a>
+        <a href="#">Events</a>
+        <a href="#">Join</a>
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
