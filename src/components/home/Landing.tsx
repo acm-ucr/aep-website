11034f5d947f7c6ca43041629caae9497369896f
snikithak <snikitha2016@gmail.com>
2026-07-07 16:43:45 +0530

    implemented the landing component in Landing.tsx

--- src/components/home/Landing.tsx
+++ src/components/home/Landing.tsx
@@ -1,5 +1,30 @@
+import Link from "next/link";
+
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <div className="flex flex-col items-center gap-10 bg-gradient-to-b from-[#A9D6FF] to-[#D8B8B7] py-20">
+      <p className="font-aep-urbanist max-w-[1069px] px-2 py-4 text-center text-[55px] font-medium text-black">
+        Preparing the next generation of environmental professionals
+        through events, mentorship, and community engagement
+      </p>
+
+      <div className="flex gap-10">
+        <Link
+          href="/about"
+          className="flex h-[150px] w-[350px] items-center justify-center rounded-[39px] bg-aep-green-200 px-2 py-4 text-[70px] font-normal text-black transition hover:brightness-95"
+        >
+          About
+        </Link>
+
+        <Link
+          href="/join"
+          className="flex h-[150px] w-[350px] items-center justify-center rounded-[39px] bg-aep-green-200 px-2 py-4 text-[70px] font-normal text-black transition hover:brightness-95"
+        >
+          Join
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Landing;
