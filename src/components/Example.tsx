9b6f8d5ebef7f03ed68e4795bbb358c0cff62cec
Chrysandra Chally <chrysandrachally@Chrysandras-MacBook-Air.local>
2026-07-07 11:29:34 -0700

    Changed code in Example.tsx and page.tsx to properly show the wanted page layout

--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -1,13 +1,25 @@
-const Example = (props: { text1: string; text2: string; text3: string }) => {
+const Example = (props: {
+  text1: string;
+  text2: string;
+  text3: string;
+  text4: string;
+}) => {
   return (
-    <div className="bg-aep-blue-400 flex w-3/4 items-center gap-1 p-10">
-      <div className="bg-aep-blue-200 w-1/2 p-5 text-left">{props.text1}</div>
-      <div className="flex w-1/2 flex-row gap-1">
-        <div className="bg-aep-blue-200 w-full p-5 text-left">
-          {props.text2}
+    <div className="bg-aep-blue-400 w-2/3 flex-col items-center gap-1 rounded-sm p-10">
+      <div className="justify-items-center">
+        <div className="bg-aep-red-100 m-8 w-1/2 p-5 text-center text-2xl">
+          {props.text1}
         </div>
-        <div className="bg-aep-blue-200 w-full p-5 text-left">
-          {props.text3}
+        <div className="flex w-1/2 flex-row gap-1">
+          <div className="bg-aep-red-200 w-full rounded-4xl p-5 text-center text-2xl">
+            {props.text2}
+          </div>
+          <div className="bg-aep-red-200 w-full rounded-4xl p-5 text-center text-2xl">
+            {props.text3}
+          </div>
+        </div>
+        <div className="bg-aep-red-400 m-8 w-1/2 p-5 text-center text-2xl text-white">
+          {props.text4}
         </div>
       </div>
     </div>
