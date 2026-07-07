115f8fc570a58f68ca8346e05d3a6ebf33b1d09f
silaswu4 <silaswu4@gmail.com>
2026-07-07 10:24:31 -0700

    Style Example component: 4-box flexbox column layout, colors, rounding, and text4 prop

--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -1,14 +1,24 @@
-const Example = (props: { text1: string; text2: string; text3: string }) => {
+const Example = (props: {
+  text1: string;
+  text2: string;
+  text3: string;
+  text4: string;
+}) => {
   return (
-    <div className="bg-aep-blue-400 flex w-3/4 items-center gap-1 p-10">
-      <div className="bg-aep-blue-200 w-1/2 p-5 text-left">{props.text1}</div>
+    <div className="bg-aep-blue-400 flex w-2/3 flex-col items-center gap-10 rounded-xl p-10">
+      <div className="bg-aep-red-100 w-1/2 p-5 text-center text-2xl text-white">
+        {props.text1}
+      </div>
       <div className="flex w-1/2 flex-row gap-1">
-        <div className="bg-aep-blue-200 w-full p-5 text-left">
+        <div className="bg-aep-red-200 w-full rounded-full p-5 text-center text-2xl">
           {props.text2}
         </div>
-        <div className="bg-aep-blue-200 w-full p-5 text-left">
+        <div className="bg-aep-red-200 w-full rounded-full p-5 text-center text-2xl">
           {props.text3}
         </div>
+      </div>
+      <div className="bg-aep-red-400 w-1/2 p-5 text-center text-2xl">
+        {props.text4}
       </div>
     </div>
   );
