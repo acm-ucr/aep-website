bb9d14bb48d9ba13a63398962c50666da5fc329f
LawMot <molawr12@gmail.com>
2026-07-07 22:57:55 -0700

    small fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,18 +4,20 @@ import AEPLogo from "@/public/Logo.webp";
 
 const Navbar = () => {
   return (
-    <div className="font-aep-urbanist fixed z-50 flex h-25 w-full items-center">
+    <div className="font-aep-urbanist fixed z-10 flex h-25 w-full items-center">
       <div className="flex flex-1 items-center p-4">
-        <Image src={AEPLogo} alt="AEP Logo" className="ml-6 size-15" />
+        <Link href="/" className="flex">
+          <Image src={AEPLogo} alt="AEP Logo" className="ml-6 size-15" />
+        </Link>
         <p className="ml-6 text-4xl font-extrabold whitespace-nowrap text-black">
           AEP
         </p>
       </div>
       <nav className="flex items-center gap-10 pr-8 text-2xl font-bold text-black">
-        <Link href="#">About</Link>
-        <Link href="#">Board</Link>
-        <Link href="#">Events</Link>
-        <Link href="#">Join</Link>
+        <Link href="/about">About</Link>
+        <Link href="/board">Board</Link>
+        <Link href="/events">Events</Link>
+        <Link href="/join">Join</Link>
       </nav>
     </div>
   );
