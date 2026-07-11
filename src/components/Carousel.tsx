36ce6d5f95ab20a8772586b0eeb6e82df3c32e97
Celina Vasquez <cvasq043@ucr.edu>
2026-07-10 18:07:18 -0700

    Implemented comments

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -9,8 +9,8 @@ type CarouselPic = {
 
 const Carousel = ({ data }: { data: CarouselPic[] }) => {
   return (
-    <div className="flex h-7/12 w-3/4 flex-col items-center justify-center bg-gray-300 p-4 overflow-hidden">
-      <div className="flex flex-row h-full w-full items-center gap-x-12">
+    <div className="flex h-7/12 w-3/4 flex-col items-center justify-center overflow-hidden bg-gray-300 p-4">
+      <div className="flex h-full w-full flex-row items-center gap-x-12">
         {data.map((item, index) => (
           <div key={index} className="h-11/12 w-4/12 shrink-0">
             <Image
