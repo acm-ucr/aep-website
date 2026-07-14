897b4634fa6632e83485388283979f865c92d501
Celina Vasquez <cvasq043@ucr.edu>
2026-07-13 22:29:01 -0700

    Socials Card

--- src/components/home/Socials.tsx
+++ src/components/home/Socials.tsx
@@ -1,5 +1,40 @@
-const Socials = () => {
-  return <div>Socials</div>;
+import { IoMail } from "react-icons/io5";
+import { RiInstagramFill } from "react-icons/ri";
+import { FaLinkedin } from "react-icons/fa";
+import SocialsCard from "./SocialsCard";
+
+interface SocialsItem {
+  name: string;
+  url: string;
+  Icon: React.ComponentType<{ className?: string }>;
+}
+
+export const SocialsLinks: SocialsItem[] = [
+  {
+    name: "Instagram",
+    url: "",
+    Icon: RiInstagramFill,
+  },
+  {
+    name: "Email",
+    url: "",
+    Icon: IoMail,
+  },
+  {
+    name: "Linkedin",
+    url: "",
+    Icon: FaLinkedin,
+  },
+];
+
+const Socials = ({ data }: { data: SocialsItem[] }) => {
+  return (
+    <div className="flex justify-center gap-x-24">
+      {data.map((item, index) => (
+        <SocialsCard key={index} mediaName={item.name} url={item.url} Icon={item.Icon} />
+      ))}
+    </div>
+  );
 };
 
 export default Socials;
--- src/components/home/SocialsCard.tsx
+++ src/components/home/SocialsCard.tsx
@@ -1,5 +1,19 @@
-const SocialsCard = () => {
-  return <div>SocialsCard</div>;
+import Link from "next/link";
+interface SocialsCard {
+  mediaName: string;
+  url: string;
+  Icon: React.ComponentType;
+}
+
+const SocialsCard = ({ mediaName, url,Icon }: SocialsCard) => {
+  return (
+    <Link href={url} className="flex flex-col h-1/12 w-2/12 justify-center">
+      <div className="flex items-center">
+        <Icon className="text-aep-green-200 w-full h-full"/>
+      </div>
+      <div className="text-4xl text-center font-bold text-aep-yellow-100 py-2">{mediaName}</div>
+    </Link>
+  );
 };
 
 export default SocialsCard;
