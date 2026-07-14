904a934fbcd285bbb7241f0d0ac74f9ea229cc05
Ronald Fan <rfan018@ucr.edu>
2026-07-13 22:33:34 -0700

    Complete BoardCard.tsx and add board.ts data file with public/Board images

--- src/components/board/BoardCard.tsx
+++ src/components/board/BoardCard.tsx
@@ -1,5 +1,23 @@
-const BoardCard = () => {
-  return <div>BoardCard</div>;
+import Image, { StaticImageData } from "next/image";
+
+interface BoardCardProps {
+  image: StaticImageData;
+  position: string;
+  name: string;
+}
+
+const BoardCard = ({ image, position, name }: BoardCardProps) => {
+  return (
+    <div className="font-aep-unna flex w-40 flex-col items-center border text-center text-lg shadow-lg">
+      <div className="h-32 w-32 overflow-hidden pt-8 pb-6">
+        <Image src={image} alt={name} className="h-full w-full object-cover scale-140" />
+      </div>
+      <div className="flex flex-col font-semibold gap-1 pb-4">
+        <p>{name}</p>
+        <p className="text-sm">{position}</p>
+      </div>
+    </div>
+  );
 };
 
 export default BoardCard;
