aac5b5579355f2b4fdbe05d99724f85f7153108e
Ronald Fan <rfan018@ucr.edu>
2026-07-13 22:34:03 -0700

    prettier

--- src/components/board/BoardCard.tsx
+++ src/components/board/BoardCard.tsx
@@ -10,9 +10,13 @@ const BoardCard = ({ image, position, name }: BoardCardProps) => {
   return (
     <div className="font-aep-unna flex w-40 flex-col items-center border text-center text-lg shadow-lg">
       <div className="h-32 w-32 overflow-hidden pt-8 pb-6">
-        <Image src={image} alt={name} className="h-full w-full object-cover scale-140" />
+        <Image
+          src={image}
+          alt={name}
+          className="h-full w-full scale-140 object-cover"
+        />
       </div>
-      <div className="flex flex-col font-semibold gap-1 pb-4">
+      <div className="flex flex-col gap-1 pb-4 font-semibold">
         <p>{name}</p>
         <p className="text-sm">{position}</p>
       </div>
