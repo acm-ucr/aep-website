9aec10242ca0e7f950f689785056668093e9947a
LawMot <molawr12@gmail.com>
2026-07-16 19:43:38 -0700

    run prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,12 @@ import Landing from "@/components/home/Landing";
 import Socials from "@/components/home/Socials";
 
 const Home = () => {
-  return (<>
-  <Landing />
-  <Socials />
-  </>);
+  return (
+    <>
+      <Landing />
+      <Socials />
+    </>
+  );
 };
 
 export default Home;
