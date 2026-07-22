757e6617c9b3d95931d15d87c39dcce0fb96cf98
Ronald Fan <rfan018@ucr.edu>
2026-07-21 23:20:18 -0700

    address comments

--- src/components/home/Landing.tsx
+++ src/components/home/Landing.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Landing = () => {
   return (
     <div className="flex flex-col items-center gap-10 py-20">
-      <p className="font-aep-urbanist max-w-6xl px-1 py-2 text-center text-4xl font-light text-black md:px-2 md:py-4 md:text-6xl md:font-medium">
+      <p className="font-aep-urbanist w-5/6 max-w-6xl px-1 py-2 text-center text-3xl font-light text-black md:px-2 md:py-4 md:text-6xl md:font-medium">
         Preparing the next generation of environmental professionals through
         events, mentorship, and community engagement
       </p>
@@ -11,14 +11,14 @@ const Landing = () => {
       <div className="flex gap-12">
         <Link
           href="/about"
-          className="bg-aep-green-200 flex h-[75px] w-[275px] items-center justify-center rounded-2xl px-1 py-2 text-[50px] font-normal text-black drop-shadow-lg transition hover:brightness-95 md:h-[150px] md:w-[350px] md:rounded-4xl md:px-2 md:py-4 md:text-[70px]"
+          className="bg-aep-green-200 flex h-20 w-32 items-center justify-center rounded-2xl px-1 py-2 text-3xl font-normal text-black drop-shadow-lg transition hover:brightness-95 md:h-[150px] md:w-[350px] md:rounded-4xl md:px-2 md:py-4 md:text-[70px]"
         >
           About
         </Link>
 
         <Link
           href="/join"
-          className="bg-aep-green-200 flex h-[75px] w-[275px] items-center justify-center rounded-2xl px-1 py-2 text-[50px] font-normal text-black drop-shadow-lg transition hover:brightness-95 md:h-[150px] md:w-[350px] md:rounded-4xl md:px-2 md:py-4 md:text-[70px]"
+          className="bg-aep-green-200 flex h-20 w-32 items-center justify-center rounded-2xl px-1 py-2 text-3xl font-normal text-black drop-shadow-lg transition hover:brightness-95 md:h-[150px] md:w-[350px] md:rounded-4xl md:px-2 md:py-4 md:text-[70px]"
         >
           Join
         </Link>
