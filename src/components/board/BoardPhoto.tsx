67c4b197fdc398296580d11617e72a0b67d8ecfb
snikithak <snikitha2016@gmail.com>
2026-07-30 11:49:15 +0530

    Made some styling changes in BoardPhoto component

--- src/components/board/BoardPhoto.tsx
+++ src/components/board/BoardPhoto.tsx
@@ -6,8 +6,8 @@ import Tape from "@/public/Board/tape.webp";
 
 const BoardPhoto = () => {
   return (
-    <div className="flex flex-col items-center">
-      <h1 className="font-aep-urbanist px-2 py-4 text-center text-7xl font-bold text-black">
+    <div className="flex flex-col items-center py-20">
+      <h1 className="font-aep-urbanist relative z-[1] px-2 py-4 text-center text-7xl font-bold text-black">
         2025-2026
       </h1>
 
@@ -16,35 +16,35 @@ const BoardPhoto = () => {
         <Image
           src={Board}
           alt="2025-2026 Board Group Photo"
-          className="relative z-10 w-4/5"
+          className="relative z-[1] w-4/5"
         />
 
         {/* Leaf */}
         <Image
           src={Leaf}
           alt="Leaf Decoration"
-          className="absolute -top-32 -right-4 z-20 w-1/4"
+          className="absolute -top-32 -right-4 z-[2] w-1/4"
         />
 
         {/* Top Left Tape */}
         <Image
           src={Tape}
           alt="Top Left Tape Decoration"
-          className="absolute -top-5 left-5 z-30 w-1/5"
+          className="absolute -top-8 left-5 z-[3] w-1/5"
         />
 
         {/* Bottom Right Tape */}
         <Image
           src={Tape}
           alt="Bottom Right Tape Decoration"
-          className="absolute right-5 -bottom-10 z-30 w-1/5"
+          className="absolute right-5 -bottom-12 z-[3] w-1/5"
         />
 
         {/* Magnifying Glass */}
         <Image
           src={MagnifyingGlass}
           alt="Magnifying Glass Decoration"
-          className="absolute -bottom-24 -left-0 z-20 w-1/4"
+          className="absolute -bottom-24 -left-0 z-[2] w-1/4"
         />
       </div>
     </div>
