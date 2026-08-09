f517945214fae8857c73af2560ab8f38b97878be
Ronald Fan <rfan018@ucr.edu>
2026-08-09 13:25:47 -0700

    minor text size change

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <header className="px-4 text-center">
-      <p className="text-3xl font-extrabold md:text-8xl">
+      <p className="text-5xl font-extrabold md:text-8xl">
         Association of Environmental Professionals @ UC Riverside
       </p>
     </header>
