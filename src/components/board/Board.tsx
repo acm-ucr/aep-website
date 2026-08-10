a8fa92d47d2e0e1e8fea18315687958bb7e3f3f9
silaswu4 <silaswu4@gmail.com>
2026-08-10 12:24:24 -0700

    address board review feedback

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -6,16 +6,16 @@ import Image from "next/image";
 
 const Board = () => {
   return (
-    <div className="relative flex flex-col items-center overflow-hidden px-6">
+    <div className="relative flex flex-col items-center overflow-hidden px-3 sm:px-6">
       <Image
         src={GridPaper}
         alt="Grid paper background"
-        className="absolute top-32 h-full w-full object-cover lg:top-56"
+        className="absolute top-32 z-0 h-[calc(100%-8rem)] w-full object-cover lg:top-56 lg:h-[calc(100%-14rem)]"
       />
-      <p className="font-aep-unna z-10 mt-16 mb-8 text-2xl font-bold sm:text-3xl md:mt-28 md:mb-14 md:text-6xl lg:mt-36 lg:mb-16 lg:text-7xl">
+      <p className="font-aep-unna z-[1] mt-16 mb-8 text-2xl font-bold sm:text-3xl md:mt-28 md:mb-14 md:text-6xl lg:mt-36 lg:mb-16 lg:text-7xl">
         Meet the Board
       </p>
-      <div className="z-10 mb-20 grid grid-cols-2 gap-x-4 gap-y-7 sm:gap-x-8 sm:gap-y-12 md:gap-x-12 md:gap-y-14 lg:grid-cols-3 lg:gap-x-14 lg:gap-y-16">
+      <div className="z-[1] mb-20 grid grid-cols-2 gap-x-2 gap-y-7 sm:gap-x-8 sm:gap-y-12 md:gap-x-12 md:gap-y-14 lg:grid-cols-3 lg:gap-x-14 lg:gap-y-16">
         {BoardProps.map((member, index) => (
           <BoardCard
             key={index}
@@ -28,7 +28,7 @@ const Board = () => {
           src={Leaf}
           alt=""
           aria-hidden="true"
-          className="z-10 w-24 self-center justify-self-center sm:w-36 md:w-44 lg:hidden"
+          className="z-[1] w-24 self-center justify-self-center sm:w-36 md:w-44 lg:hidden"
         />
       </div>
     </div>
--- src/components/board/BoardCard.tsx
+++ src/components/board/BoardCard.tsx
@@ -8,8 +8,8 @@ interface BoardCardProps {
 
 const BoardCard = ({ image, position, name }: BoardCardProps) => {
   return (
-    <div className="font-aep-unna flex min-h-36 w-20 flex-col items-center border bg-white text-center shadow-2xl sm:min-h-60 sm:w-36 md:min-h-72 md:w-44 lg:min-h-80 lg:w-48">
-      <div className="mt-2 h-16 w-16 sm:mt-5 sm:h-28 sm:w-28 md:h-36 md:w-36 lg:mt-6 lg:h-40 lg:w-40">
+    <div className="font-aep-unna flex min-h-44 w-24 flex-col items-center border bg-white text-center shadow-2xl sm:min-h-64 sm:w-40 md:min-h-72 md:w-44 lg:min-h-80 lg:w-48">
+      <div className="mt-3 h-20 w-20 sm:mt-5 sm:h-32 sm:w-32 md:h-36 md:w-36 lg:mt-6 lg:h-40 lg:w-40">
         <Image src={image} alt={name} className="h-full w-full object-cover" />
       </div>
       <div className="flex flex-col px-1 pt-2 text-sm leading-tight sm:text-xl md:text-2xl">
