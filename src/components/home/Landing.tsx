1a8dd873c6a48d22eef095621e790ff871dc738e
Chrysandra Chally <chrysandrachally@Chrysandras-MacBook-Air.local>
2026-08-16 16:55:16 -0700

    Added animation to landing page

--- src/components/home/Landing.tsx
+++ src/components/home/Landing.tsx
@@ -1,4 +1,6 @@
+"use client";
 import Link from "next/link";
+import { motion } from "motion/react";
 
 const Landing = () => {
   return (
@@ -9,19 +11,23 @@ const Landing = () => {
       </p>
 
       <div className="flex gap-12">
-        <Link
-          href="/about"
-          className="bg-aep-green-200 flex h-20 w-32 items-center justify-center rounded-2xl px-1 py-2 text-3xl font-normal text-black drop-shadow-lg transition hover:brightness-95 md:h-[150px] md:w-[350px] md:rounded-4xl md:px-2 md:py-4 md:text-[70px]"
-        >
-          About
-        </Link>
+        <motion.div whileHover={{ scale: 1.07 }}>
+          <Link
+            href="/about"
+            className="bg-aep-green-200 flex h-20 w-32 items-center justify-center rounded-2xl px-1 py-2 text-3xl font-normal text-black drop-shadow-lg transition hover:brightness-95 md:h-[150px] md:w-[350px] md:rounded-4xl md:px-2 md:py-4 md:text-[70px]"
+          >
+            About
+          </Link>
+        </motion.div>
 
-        <Link
-          href="/join"
-          className="bg-aep-green-200 flex h-20 w-32 items-center justify-center rounded-2xl px-1 py-2 text-3xl font-normal text-black drop-shadow-lg transition hover:brightness-95 md:h-[150px] md:w-[350px] md:rounded-4xl md:px-2 md:py-4 md:text-[70px]"
-        >
-          Join
-        </Link>
+        <motion.div whileHover={{ scale: 1.07 }}>
+          <Link
+            href="/join"
+            className="bg-aep-green-200 flex h-20 w-32 items-center justify-center rounded-2xl px-1 py-2 text-3xl font-normal text-black drop-shadow-lg transition hover:brightness-95 md:h-[150px] md:w-[350px] md:rounded-4xl md:px-2 md:py-4 md:text-[70px]"
+          >
+            Join
+          </Link>
+        </motion.div>
       </div>
     </div>
   );
