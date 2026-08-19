5fa16fe283a7282305d43897d4626991f39cc858
Chrysandra Chally <chrysandrachally@Chrysandras-MacBook-Air.local>
2026-08-19 16:52:43 -0700

    Implemented Board Animation

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -1,8 +1,10 @@
+"use client";
 import BoardCard from "./BoardCard";
 import { BoardProps } from "@/data/board";
 import Leaf from "@/public/Board/leaf.webp";
 import GridPaper from "@/public/Board/paper.webp";
 import Image from "next/image";
+import { motion } from "motion/react";
 
 const Board = () => {
   return (
@@ -12,10 +14,23 @@ const Board = () => {
         alt="Grid paper background"
         className="absolute top-32 z-0 h-[calc(100%-8rem)] w-full object-cover lg:top-56 lg:h-[calc(100%-14rem)]"
       />
-      <p className="font-aep-unna z-[1] mt-16 mb-8 text-2xl font-bold sm:text-3xl md:mt-28 md:mb-14 md:text-6xl lg:mt-36 lg:mb-16 lg:text-7xl">
-        Meet the Board
-      </p>
-      <div className="z-[1] mb-20 grid grid-cols-2 gap-x-2 gap-y-7 sm:gap-x-8 sm:gap-y-12 md:gap-x-12 md:gap-y-14 lg:grid-cols-3 lg:gap-x-14 lg:gap-y-16">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        whileHover={{ scale: 1.08 }}
+        transition={{ duration: 0.5 }}
+      >
+        <p className="font-aep-unna z-[1] mt-16 mb-8 text-2xl font-bold sm:text-3xl md:mt-28 md:mb-14 md:text-6xl lg:mt-36 lg:mb-16 lg:text-7xl">
+          Meet the Board
+        </p>
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, y: -25 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+        viewport={{ once: true }}
+        className="z-[1] mb-20 grid grid-cols-2 gap-x-2 gap-y-7 sm:gap-x-8 sm:gap-y-12 md:gap-x-12 md:gap-y-14 lg:grid-cols-3 lg:gap-x-14 lg:gap-y-16"
+      >
         {BoardProps.map((member, index) => (
           <BoardCard
             key={index}
@@ -24,13 +39,14 @@ const Board = () => {
             name={member.name}
           />
         ))}
+
         <Image
           src={Leaf}
           alt=""
           aria-hidden="true"
           className="z-[1] w-24 self-center justify-self-center sm:w-36 md:w-44 lg:hidden"
         />
-      </div>
+      </motion.div>
     </div>
   );
 };
