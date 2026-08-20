47364493eaef11c54b550221cc10859b55c10df0
Celina Vasquez <cvasq043@ucr.edu>
2026-08-19 20:01:35 -0700

    Mobile responsiveness for join page completed

--- src/components/join/Join.tsx
+++ src/components/join/Join.tsx
@@ -11,9 +11,9 @@ const Join = () => {
   const GROUPME_URL = "https://groupme.com/join_group/109731630/ApjU7A0t";
 
   return (
-    <div className="bg-aep-sand-500 relative flex max-h-screen md:min-h-[170vh] flex-col items-center justify-start">
-      <div className="from-aep-rose-300 via-aep-peach-300 to-aep-yellow-400 absolute inset-x-0 top-0 z-0 h-3/5 md:h-[66.6%] bg-gradient-to-b" />
-      <div className="absolute top-72 pt-16 md:pt-0 md:top-3/10 z-10 h-20 w-20 md:h-50 md:w-50">
+    <div className="bg-aep-sand-500 relative flex max-h-screen flex-col items-center justify-start md:min-h-[170vh]">
+      <div className="from-aep-rose-300 via-aep-peach-300 to-aep-yellow-400 absolute inset-x-0 top-0 z-0 h-3/5 bg-gradient-to-b md:h-[66.6%]" />
+      <div className="absolute top-72 z-10 h-20 w-20 pt-16 md:top-3/10 md:h-50 md:w-50 md:pt-0">
         <Image src={Sun} alt="Sun" fill className="object-contain" />
       </div>
 
@@ -22,17 +22,17 @@ const Join = () => {
           src={Trees}
           alt="Trees"
           fill
-          className="object-contain mt-20 md:mt-0 md:object-bottom"
+          className="mt-20 object-contain md:mt-0 md:object-bottom"
         />
       </div>
 
       <div className="relative mt-40 px-4 text-center">
-        <h1 className="font-aep-unna text-6xl md:text-9xl font-bold text-white">
+        <h1 className="font-aep-unna text-6xl font-bold text-white md:text-9xl">
           Get Involved
         </h1>
       </div>
 
-      <div className="md:absolute pl-10 md:pl-0 items-center justify-center md:justify-evenly md:inset-x-0 pt-72 md:pt-0 flex flex-col md:flex-row pb-16 md:pb-0 md:bottom-5 z-8 md:mx-auto max-w-7xl md:space-x-20">
+      <div className="z-8 flex max-w-7xl flex-col items-center justify-center pt-72 pb-16 pl-10 md:absolute md:inset-x-0 md:bottom-5 md:mx-auto md:flex-row md:justify-evenly md:space-x-20 md:pt-0 md:pb-0 md:pl-0">
         <div className="pointer-events-auto relative mt-8 h-36 w-44 md:mt-0 md:h-120 md:w-120">
           <Link href={JOIN_FORM_URL} target="_blank" rel="noopener noreferrer">
             <Image
@@ -41,8 +41,8 @@ const Join = () => {
               fill
               className="object-contain"
             />
-            <div className="absolute inset-0 flex items-center justify-center mr-4 -mt-14 md:mt-0 md:mr-0 md:pr-4 md:pb-45">
-              <span className="font-aep-urbanist text-aep-yellow-500 text-2xl md:text-8xl font-bold">
+            <div className="absolute inset-0 -mt-14 mr-4 flex items-center justify-center md:mt-0 md:mr-0 md:pr-4 md:pb-45">
+              <span className="font-aep-urbanist text-aep-yellow-500 text-2xl font-bold md:text-8xl">
                 Join Here
               </span>
             </div>
@@ -55,10 +55,10 @@ const Join = () => {
               src={RightSign}
               alt="GroupMe Sign"
               fill
-              className="object-contain -mt-8 -ml-10 md:mt-0 md:ml-0"
+              className="-mt-8 -ml-10 object-contain md:mt-0 md:ml-0"
             />
-            <div className="absolute inset-0 flex items-center justify-center mr-16 -mt-28 md:mt-0 md:mr-0 md:pb-45 md:pl-7">
-              <span className="font-aep-urbanist text-aep-yellow-500 text-2xl md:text-8xl font-bold">
+            <div className="absolute inset-0 -mt-28 mr-16 flex items-center justify-center md:mt-0 md:mr-0 md:pb-45 md:pl-7">
+              <span className="font-aep-urbanist text-aep-yellow-500 text-2xl font-bold md:text-8xl">
                 GroupMe
               </span>
             </div>
