fed418de9b502f9831d8df4928928c1ddaa7c798
etan29 <emilytan2992@gmail.com>
2026-08-31 05:11:15 -0700

    fix socials desktop background

--- src/components/home/Socials.tsx
+++ src/components/home/Socials.tsx
@@ -32,10 +32,14 @@ export const SocialsLinks: SocialsItem[] = [
 
 const Socials = () => {
   return (
-    <div className="relative pb-40 md:pb-72">
-      <div className="relative flex min-h-[32rem] flex-col items-center justify-center md:min-h-screen">
-        <Image className="object-cover" src={background} alt="socials" fill />
-        <div className="font-aep-urbanist relative px-8 pb-8 text-center text-xl font-bold text-white md:px-4 md:pb-45 md:text-5xl">
+    <div className="relative min-h-[32rem] md:min-h-0">
+      <Image
+        className="absolute inset-0 h-full w-full object-cover md:static md:h-auto"
+        src={background}
+        alt="socials"
+      />
+      <div className="relative flex min-h-[32rem] flex-col items-center justify-center md:absolute md:inset-0 md:min-h-0 md:justify-end md:pb-28">
+        <div className="font-aep-urbanist relative px-8 pb-8 text-center text-xl font-bold text-white md:px-4 md:pb-32 md:text-5xl lg:pb-96">
           <p>
             ...to enhance, maintain and protect the quality of the natural and
             human environment.
